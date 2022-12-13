6eea7ce3d10592f14a1a3bf3fb8dd8c2b20764b3
mazari <mazari.o@edu.spbstu.ru>
2022-12-13 21:46:27 +0300

    1 2 3 VIVA L"algerie

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -89,8 +89,9 @@ data class Circle(val center: Point, val radius: Double) {
      *
      * Вернуть true, если и только если окружность содержит данную точку НА себе или ВНУТРИ себя
      */
-    fun contains(p: Point): Boolean = TODO()
+    fun contains(p: Point): Boolean = p.distance(this.center) <= radius
 }
+
 
 /**
  * Отрезок между двумя точками
