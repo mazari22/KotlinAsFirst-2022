e953dd7054f6cecf230cb5c4335d0b857811155f
Alexey-Gaidai <1123_666@mail.ru>
2022-12-13 23:52:50 +0700

    lesson 7 & 12

--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -18,13 +18,23 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    private val contact = mutableMapOf<String, MutableSet<String>>()
+    private val phones = mutableMapOf<String, String>()
+
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean {
+        return if (contact.contains(name))
+            false
+        else {
+            contact[name] = mutableSetOf()
+            true
+        }
+    }
 
     /**
      * Убрать человека.
@@ -32,7 +42,17 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean {
+        return if (!contact.containsKey(name))
+            false
+        else {
+            contact[name]!!.forEach { phoneNumber ->
+                phones.remove(phoneNumber)
+            }
+            contact.remove(name)
+            return true
+        }
+    }
 
     /**
      * Добавить номер телефона.
@@ -41,7 +61,15 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean {
+        return if (!contact.containsKey(name) || phones.containsKey(phone))
+            false
+        else {
+            contact[name]!!.add(phone)
+            phones[phone] = name
+            return true
+        }
+    }
 
     /**
      * Убрать номер телефона.
@@ -49,24 +77,40 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean {
+        return if (!contact.containsKey(name) || !phones.containsKey(phone))
+            false
+        else {
+            contact[name]!!.remove(phone)
+            phones.remove(phone)
+            return true
+        }
+    }
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> {
+        return if (!contact.containsKey(name))
+            emptySet()
+        else {
+            return contact[name]!!.toSet()
+        }
+    }
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? = phones[phone]
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is PhoneBook && contact == other.contact
+
+    override fun hashCode(): Int = contact.hashCode() + phones.hashCode()
 }
--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -63,8 +63,21 @@ fun alignFile(inputName: String, lineLength: Int, outputName: String) {
  * Подчёркивание в середине и/или в конце строк значения не имеет.
  */
 fun deleteMarked(inputName: String, outputName: String) {
-    TODO()
+    //райтер для записи в файл
+    val writer = File(outputName).bufferedWriter()
+    //циклом проходим по всем строкам в файле и записываем их в райтер
+    //если строка начинается с _ то пропускаем её иначе записываем
+    for (line in File(inputName).readLines())
+        File(inputName).forEachLine {
+            if (!it.startsWith("_")) {
+                writer.write(it)
+                writer.newLine()
+            }
+        }
+    //закрываем райтер
+    writer.close()
 }
+
 
 /**
  * Средняя (14 баллов)
@@ -92,7 +105,23 @@ fun countSubstrings(inputName: String, substrings: List<String>): Map<String, In
  *
  */
 fun sibilants(inputName: String, outputName: String) {
-    TODO()
+    val replaces = mapOf( //создаем ассоциативный список заменяемых букв
+        'Ы' to 'И', 'Я' to 'А', 'Ю' to 'У',
+        'ы' to 'и', 'я' to 'а', 'ю' to 'у'
+    )
+    var text = File(inputName).readText()//считываем текст
+    File(outputName).bufferedWriter().use {
+        for (chars in text.indices) {//проходим по каждому символу в строке
+            if (text[chars] in replaces && text[chars - 1] in "жчшщЖЧШЩ") {//если символ у нас есть в replaces и предыдущая буква жчшщ
+                text = text.replaceRange(//то мы заменяем на правильные буквы
+                    startIndex = chars,
+                    endIndex = chars + 1,
+                    "${replaces[text[chars]]}"
+                )
+            }
+        }
+        it.write(text)//записываем в файл
+    }
 }
 
 /**
@@ -113,7 +142,19 @@ fun sibilants(inputName: String, outputName: String) {
  *
  */
 fun centerFile(inputName: String, outputName: String) {
-    TODO()
+    val lines = File(inputName).readLines() //читаем строки
+    File(outputName).bufferedWriter().use { // используем райтер
+        when (lines) {
+            emptyList<String>() -> it.write("")// если строка пустая, то пишем пустую строку
+            else -> { //иначе
+                val maxLength = lines.maxOf { it.trim().length }//мы определяем максимальную длину строки
+                lines.forEach { str -> //затем на каждую строчку
+                    it.write(" ".repeat((maxLength - str.trim().length) / 2))//мы пишем пробелы в том количестве которое нужно для того чтобы уровнять
+                    it.write(str.trim() + "\n")//затем пишем строчку и переносим райтер на новую строку
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -144,7 +185,6 @@ fun centerFile(inputName: String, outputName: String) {
  * 8) Если входной файл удовлетворяет требованиям 1-7, то он должен быть в точности идентичен выходному файлу
  */
 fun alignFileByWidth(inputName: String, outputName: String) {
-    TODO()
 }
 
 /**
@@ -268,15 +308,15 @@ Suspendisse ~~et elit in enim tempus iaculis~~.
  *
  * Соответствующий выходной файл:
 <html>
-    <body>
-        <p>
-            Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
-            Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
-        </p>
-        <p>
-            Suspendisse <s>et elit in enim tempus iaculis</s>.
-        </p>
-    </body>
+<body>
+<p>
+Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
+Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
+</p>
+<p>
+Suspendisse <s>et elit in enim tempus iaculis</s>.
+</p>
+</body>
 </html>
  *
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
@@ -319,65 +359,65 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
  *
  * Пример входного файла:
 ///////////////////////////////начало файла/////////////////////////////////////////////////////////////////////////////
-* Утка по-пекински
-    * Утка
-    * Соус
-* Салат Оливье
-    1. Мясо
-        * Или колбаса
-    2. Майонез
-    3. Картофель
-    4. Что-то там ещё
-* Помидоры
-* Фрукты
-    1. Бананы
-    23. Яблоки
-        1. Красные
-        2. Зелёные
+ * Утка по-пекински
+ * Утка
+ * Соус
+ * Салат Оливье
+1. Мясо
+ * Или колбаса
+2. Майонез
+3. Картофель
+4. Что-то там ещё
+ * Помидоры
+ * Фрукты
+1. Бананы
+23. Яблоки
+1. Красные
+2. Зелёные
 ///////////////////////////////конец файла//////////////////////////////////////////////////////////////////////////////
  *
  *
  * Соответствующий выходной файл:
 ///////////////////////////////начало файла/////////////////////////////////////////////////////////////////////////////
 <html>
-  <body>
-    <p>
-      <ul>
-        <li>
-          Утка по-пекински
-          <ul>
-            <li>Утка</li>
-            <li>Соус</li>
-          </ul>
-        </li>
-        <li>
-          Салат Оливье
-          <ol>
-            <li>Мясо
-              <ul>
-                <li>Или колбаса</li>
-              </ul>
-            </li>
-            <li>Майонез</li>
-            <li>Картофель</li>
-            <li>Что-то там ещё</li>
-          </ol>
-        </li>
-        <li>Помидоры</li>
-        <li>Фрукты
-          <ol>
-            <li>Бананы</li>
-            <li>Яблоки
-              <ol>
-                <li>Красные</li>
-                <li>Зелёные</li>
-              </ol>
-            </li>
-          </ol>
-        </li>
-      </ul>
-    </p>
-  </body>
+<body>
+<p>
+<ul>
+<li>
+Утка по-пекински
+<ul>
+<li>Утка</li>
+<li>Соус</li>
+</ul>
+</li>
+<li>
+Салат Оливье
+<ol>
+<li>Мясо
+<ul>
+<li>Или колбаса</li>
+</ul>
+</li>
+<li>Майонез</li>
+<li>Картофель</li>
+<li>Что-то там ещё</li>
+</ol>
+</li>
+<li>Помидоры</li>
+<li>Фрукты
+<ol>
+<li>Бананы</li>
+<li>Яблоки
+<ol>
+<li>Красные</li>
+<li>Зелёные</li>
+</ol>
+</li>
+</ol>
+</li>
+</ul>
+</p>
+</body>
 </html>
 ///////////////////////////////конец файла//////////////////////////////////////////////////////////////////////////////
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
@@ -404,23 +444,23 @@ fun markdownToHtml(inputName: String, outputName: String) {
  * Вывести в выходной файл процесс умножения столбиком числа lhv (> 0) на число rhv (> 0).
  *
  * Пример (для lhv == 19935, rhv == 111):
-   19935
-*    111
+19935
+ *    111
 --------
-   19935
+19935
 + 19935
 +19935
 --------
- 2212785
+2212785
  * Используемые пробелы, отступы и дефисы должны в точности соответствовать примеру.
  * Нули в множителе обрабатывать так же, как и остальные цифры:
-  235
-*  10
+235
+ *  10
 -----
-    0
+0
 +235
 -----
- 2350
+2350
  *
  */
 fun printMultiplicationProcess(lhv: Int, rhv: Int, outputName: String) {
@@ -434,16 +474,16 @@ fun printMultiplicationProcess(lhv: Int, rhv: Int, outputName: String) {
  * Вывести в выходной файл процесс деления столбиком числа lhv (> 0) на число rhv (> 0).
  *
  * Пример (для lhv == 19935, rhv == 22):
-  19935 | 22
- -198     906
- ----
-    13
-    -0
-    --
-    135
-   -132
-   ----
-      3
+19935 | 22
+-198     906
+----
+13
+-0
+--
+135
+-132
+----
+3
 
  * Используемые пробелы, отступы и дефисы должны в точности соответствовать примеру.
  *
